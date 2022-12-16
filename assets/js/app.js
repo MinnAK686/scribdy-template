5f4cfb8aa7b74ffbbcadea389aa3d183d17c627c
MinnAK686 <anastasioak327@gmail.com>
2022-12-16 21:34:52 +0630

    Design navbar and mobile button tab bar and add footer

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,45 @@
 "use strict";
 
+
+
+let showProfile = document.querySelector(".profile-info-container");
+let showProfileInfo = () => {
+    if(showProfile.style.display == "block") showProfile.style.display = "none"
+    else showProfile.style.display = "block"
+}
+
+document.querySelector("body").addEventListener("click", () => {
+    showProfileInfo.style.display = "none !important"
+})
+
+let backuser = document.querySelector(".back-user");
+backuser.style.display = "none"
+let toggleOffcanvas = document.querySelector(".toggleOffcanvas");
+let offcanvasToggler = document.querySelector(".offcanvas-toggler");
+let showMobileProfile = document.querySelector(".mobile-profile-info-container")
+let showMobileProfileInfo = () => {
+    if(showMobileProfile.style.display == "block") {
+        showMobileProfile.style.display = "none"
+        offcanvasToggler.style.display = "block"
+        backuser.style.display = "none"
+        toggleOffcanvas.style.display = "block"
+    }else {
+        showMobileProfile.style.display = "block"
+        offcanvasToggler.style.display = "none"
+        backuser.style.display = "block"
+        toggleOffcanvas.style.display = "none"
+    }
+}
+let backOffcanvas = () => {
+    if(showMobileProfile.style.display == "block") {
+        showMobileProfile.style.display = "none"
+        offcanvasToggler.style.display = "block"
+        backuser.style.display = "none"
+        toggleOffcanvas.style.display = "block"
+    }else {
+        showMobileProfile.style.display = "block"
+        offcanvasToggler.style.display = "none"
+        backuser.style.display = "block"
+        toggleOffcanvas.style.display = "none"
+    }
+}
